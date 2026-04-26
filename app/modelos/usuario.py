fd6b878a5f506d45458ffb7cf869fd4a565e7f2f
Christian <escalasnavarro@gmail.com>
2026-04-26 12:25:17 +0200

    feat: creación de los modelos de la base de datos

---
+++ app/modelos/usuario.py
@@ -0,0 +1,12 @@
+from db import db
+
+class Usuario(db.Model):
+  __tablename__ = "usuarios"
+  
+  id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+  nombre = db.Column(db.String(30), nullable = False)
+  apellidos = db.Column(db.String(50), nullable = False)
+  email = db.Column(db.String(100), unique = True, nullable = False)
+  password = db.Column(db.String(255), nullable = False)
+  
+  profesores = db.relationship("Profesor", back_populates = "usuario")
